48c6799bcfb711dbe8ed5114d22c49be23259b31
pc <omarelibrahimi567@gmail.com>
2024-06-19 21:49:20 +0100

    second commit

--- src/main/java/dev/example/kinect/workflow/ProfileWorkflow.java
+++ src/main/java/dev/example/kinect/workflow/ProfileWorkflow.java
@@ -20,5 +20,7 @@ public interface ProfileWorkflow {
     RequestDTO createRequest(RequestDTO requestDTO, Long profile_id) throws ProfileNotFoundException, OfferNotFoundException;
     String acceptRequest(Long request_id) throws RequestNotFoundException, ProfileNotFoundException;
     String deniedRequest(Long request_id) throws RequestNotFoundException;
-    String cancelRequest(Long request_id) throws RequestNotFoundException;
+    OfferDTO updateOffer(Long offerId, OfferDTO offerDTO, Long profileId)
+            throws OfferNotFoundException, PlanningNotFoundException, ProfileNotFoundException;
+
 }
